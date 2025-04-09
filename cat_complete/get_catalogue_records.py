4399a5484c5552f31eea684eae4597d841b4a098
Sam Pepler <sam.pepler@stfc.ac.uk>
2025-04-09 10:02:16 +0100

    as found

--- cat_complete/get_catalogue_records.py
+++ cat_complete/get_catalogue_records.py
@@ -3,19 +3,30 @@ import json
 import requests
 import time
 
-def get_catalogue_record_paths(cache_filename="catalogue_record_paths_cache.json"):
-    url = 'http://api.catalogue.ceda.ac.uk/api/v2/observations/?ordering=uuid'
-    url = 'http://api.catalogue.ceda.ac.uk/api/v1/observations.json'
-    if os.path.exists(cache_filename) and os.path.getmtime(cache_filename) > time.time() - 24*3600:
+def get_catalogue_record_paths(cache_filename="catalogue_record_paths_cache.json", cache_ttl=24*3600):
+    """Get catalogue record information from the moles API. Grabs all records from the catalogue and stores then in a cache JSON file."""
+    
+    # too detailed - crashes server
+    # url = 'http://api.catalogue.ceda.ac.uk/api/v2/observations.json?limit=200'  
+   
+    # needs doing in one lump. pagination does not work.
+    url = 'http://api.catalogue.ceda.ac.uk/api/v1/observations.json?limit=12000&offset=0'  
+    
+    if os.path.exists(cache_filename) and os.path.getmtime(cache_filename) > time.time() - cache_ttl:
         return json.load(open(cache_filename))
 
     paths = {}
+    i = 0 
     while url is not None:
         r = requests.get(url=url)
+        print(r)
         record_list = r.json()
         for record in record_list["results"]:
+            i += 1
             # print(record)
             result_field = record["result_field"]
+            if record["uuid"] == "bdade57677db14829c6cdaa1f3e937c9":
+                print("****** FOUND bdade57677db14829c6cdaa1f3e937c9")
             # print(result_field)
             if result_field is None: 
                 print(f"'{record['title']}' result_field is None")
@@ -28,15 +39,18 @@ def get_catalogue_record_paths(cache_filename="catalogue_record_paths_cache.json
                 continue
 
             path = result_field["dataPath"].rstrip("/")
+            if path in paths:
+                print(f"DUPLICATE ----- {path}")
             paths[path] = record["publicationState"]
 
            
         url = record_list["next"]
-        print(len(paths), path, url) 
+        print(len(paths), path, url, i) 
 
     json.dump(paths, open(cache_filename, "w"), indent=4)
     return paths
 
 if __name__ == "__main__":
-    x = get_catalogue_record_paths()
+    x = get_catalogue_record_paths(cache_ttl=0)
+    print(len(x))
 
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. Archive Completeness Checker documentation master file, created by
+   sphinx-quickstart on Thu Apr 13 09:18:51 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Archive Completeness Checker's documentation!
+========================================================
+
+This software is used to work out which bits of the CEDA Archive need catalogue records. 
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. autofunction:: cat_complete.get_catalogue_records.get_catalogue_record_paths
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
